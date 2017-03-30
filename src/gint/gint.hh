fdab17385ac4bd86c7591132ffdd74928eb6836e
Michael F. Herbst <info@michael-herbst.com>
2017-03-30 14:36:58 +0200

    Public header
    This header file includes everything which is needed to link to gint.

---
+++ src/gint/gint.hh
@@ -0,0 +1,35 @@
+//
+// Copyright (C) 2017 by the gint authors
+//
+// This file is part of gint.
+//
+// gint is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// gint is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with gint. If not, see <http://www.gnu.org/licenses/>.
+//
+
+#pragma once
+/// \file Header file defining the public interface of gint
+
+#include "version.hh"
+
+// Integrals
+#include "Integral.hh"
+#include "IntegralCollectionBase.hh"
+#include "IntegralIdentifier.hh"
+#include "IntegralLookup.hh"
+#include "IntegralType.hh"
+#include "OrbitalType.hh"
+
+// Chemistry
+#include "Atom.hh"
+#include "Structure.hh"
