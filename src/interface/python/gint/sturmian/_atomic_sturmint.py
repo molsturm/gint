786ddde8b34b9cc28f833fc15b85dc01ff2e86bb
Michael F. Herbst <info@michael-herbst.com>
2017-08-13 19:40:13 +0200

    BUGFIX: File had the wrong name

---
+++ src/interface/python/gint/sturmian/_atomic_sturmint.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+## vi: tabstop=4 shiftwidth=4 softtabstop=4 expandtab
+## ---------------------------------------------------------------------
+##
+## Copyright (C) 2017 by the gint authors
+##
+## This file is part of gint.
+##
+## gint is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published
+## by the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+##
+## gint is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with gint. If not, see <http://www.gnu.org/licenses/>.
+##
+## ---------------------------------------------------------------------
+
+from .._available_basis_types import available_basis_types
+import sturmint.atomic.cs
+
+"""The list of available sturmian backends"""
+available_backends = [t[9:] for t in available_basis_types if t.startswith("sturmian/")]
+
+
+print("WARNING:  sturmian Basis structure is not yet properly implemented.")
+
+
+class Basis(sturmint.atomic.cs.Basis):
+    def __init__(self, structure, k_exp, n_max, l_max=None, m_max=None, backend="auto"):
+        if backend == "auto":
+            # List the priority of the backends
+            __backend_priority = ["cs_reference_pc", "cs_dummy", "cs_static14"]
+
+            for b in __backend_priority + available_backends:
+                if b in available_backends:
+                    self.backend = b
+                    break
+        else:
+            if backend not in available_backends:
+                raise ValueError("The gaussian inegral backend " + backend +
+                                 " is not available. The following basis types "
+                                 "are implemented: " +
+                                 ",".join(available_basis_types))
+            self.backend = backend
+
+        # TODO If the backend does not use nlm order, change that here.
+        super().__init__(k_exp, n_max, l_max, m_max, order="nlm")
+
+    @property
+    def has_real_harmonics():
+        """
+        Does this basis have real functions to describe the angular part. This impies that
+        the repulsion integrals satisfy the extra symmetry (ab|cd) = (ba|cd) in shell
+        pair notation, which would not be true otherwise
+        """
+        raise NotImplementedError()
+
+    @property
+    def size(self):
+        """Return the number of basis functions in this basis"""
+        return self.__len__()
+
+
+
+
+    @property
+    def basis_type(self):
+        return "sturmian/atomic/" + self.backend
+
