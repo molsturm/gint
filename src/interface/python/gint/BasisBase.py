fd0ebd8f2034fc970de90ddea6c970c6a10bf21b
Michael F. Herbst <info@michael-herbst.com>
2017-10-08 23:46:00 +0200

    Forgotten python script

---
+++ src/interface/python/gint/BasisBase.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+## vi: tabstop=4 shiftwidth=4 softtabstop=4 expandtab
+## ---------------------------------------------------------------------
+##
+## Copyright (C) 2017 by the gint authors
+##
+## This file is part of gint.
+##
+## gint is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published
+## by the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+##
+## gint is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with gint. If not, see <http://www.gnu.org/licenses/>.
+##
+## ---------------------------------------------------------------------
+
+from . import _iface as iface
+
+""" The list of available basis types """
+available_basis_types = tuple(iface.available_basis_types())
+
+
+class BasisBase:
+    """
+    The base class for all basis classes in gint
+    """
+    pass
